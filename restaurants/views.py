ecd0904d533cc6a4f7e89f6da8fa5f08d7cc22bc
Sazida Hossain <sazidahossain@Sazidas-MacBook-Pro.local>
2018-10-07 16:00:56 +0300

    Task 3 Submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,16 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
-    context = {
-
-    }
+    restaurants=[{'restaurant_name':'Potatoes','food_type':'All things potatoes!'},{'restaurant_name':'Steakhouse','food_type':'All things meat!'},{'restaurant_name':'Mexicana','food_type':'Mexican'}]
+    context = {"my_list": restaurants}
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
-
-    }
+ "my_object": {
+            "restaurant_name":"Mexicana",
+            'food_type':"Mexican"
+    }}
     return render(request, 'detail.html', context)
